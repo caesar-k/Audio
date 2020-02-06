ae9c5424db65cff0c6457320d4ff99e5f331872e
Chang Ze-Rong <kchang1@imsa.edu>
2020-02-06 13:32:10 -0600

    deleted unnecessary code in polyphony1

--- ece297/polyphony1/polyphony1.ino
+++ ece297/polyphony1/polyphony1.ino
@@ -1,3 +1,9 @@
+/*
+* Just to test that the Teensy can play 2 tones simultaneously, 
+* No buttons 
+* Should play automatically 
+*/
+
 
 #include <Audio.h>
 #include <Wire.h>
@@ -16,44 +22,41 @@ AudioConnection          patchCord3(waveform2, 0, i2s1, 1);
 AudioConnection          patchCord4(waveform2, 0, dacs1, 1);
 AudioControlSGTL5000     sgtl5000_1;     //xy=239,232
 
-Bounce button0 = Bounce(0, 15);
-Bounce button1 = Bounce(1, 15);
-Bounce button2 = Bounce(2, 15);
-
-int current_waveform=0;
-
+// amplitude/volume of waveform when on 
 const int onamp = 10;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(0, INPUT_PULLUP);
-  pinMode(1, INPUT_PULLUP);
-  pinMode(2, INPUT_PULLUP);
 
   // Audio connections require memory to work.  For more
   // detailed information, see the MemoryAndCpuUsage example
   AudioMemory(10);
 
-  // Comment these out if not using the audio adaptor board.
-  // This may wait forever if the SDA & SCL pins lack
-  // pullup resistors
   sgtl5000_1.enable();
-  sgtl5000_1.volume(0.1); // caution: very loud - use oscilloscope only!
+  sgtl5000_1.volume(0.1); 
   
   waveform1.begin(WAVEFORM_SINE);
   waveform2.begin(WAVEFORM_SINE);
 
+  // sets frequency of sine wave 
   waveform1.frequency(262);
   waveform2.frequency(392);
 }
 
-void loop() {
+void loop() { 
+  // waveform1 on 
   waveform1.amplitude(onamp);
   delay(500);
+  
+  // waveform1 & 2 on 
   waveform2.amplitude(onamp);
   delay(500);
+
+  // waveform1 off 
   waveform1.amplitude(0);
   delay(500);
+
+  // waveform2 off 
   waveform2.amplitude(0);
   delay(500);
 }
